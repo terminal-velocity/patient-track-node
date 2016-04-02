4fdc25631bfa734325fdaf7b4eb97ec87ff6ed9c
isaac.reid.guest@gmail.com <isaac.reid.guest@gmail.com>
2016-04-02 17:14:42 +0100

    Mega changes

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,49 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/admin/patients', function(req, res) {
+  var patients = fs.readdirSync("./data/patients");
+  console.log(patients);
+  res.render('patients', {
+    patients: patients
+  });
+});
+
+router.get('/admin/patients/:id/raw', function(req, res) {
+  var patient = fs.readFileSync("./data/patients/"+req.params.id);
+  console.log(patient);
+  res.send(patient.toString());
+});
+
+router.post('/admin/patients', function(req, res) {
+  console.log(req.body.id);
+  console.log(req.body.payload);
+  console.log(req.body.form);
+  var path = "data/patients/"+req.body.id+".json"
+  var payload = req.body.payload.toString();
+  console.log(payload.toString());
+  console.log(path);
+  fs.writeFile(path,payload,function(err){
+    if(err) throw err;
+    console.log("Saved");
+    res.redirect("/editor/"+req.body.form+"/"+req.body.id);
+  });
+});
+/*
+router.get('/admin/patients/:id', function(req, res) {
+  var patients = fs.readFileSync("./data/patients/"+req.params.id);
+  if(form){
+    form = form.toString();
+    console.log(form);
+    res.render('admin-form-edit', {
+      formname: req.params.id,
+      form: form
+    });
+  }else{
+    res.send("~");
+  }
+});*/
+
 router.get('/admin/forms', function(req, res) {
   var forms = fs.readdirSync("./public/forms");
   console.log(forms);
@@ -39,5 +82,14 @@ router.post('/admin/forms', function(req, res) {
   }
 });
 
+
+router.get("/admin/forceClonetest", function(req, res){
+  var Git = require("nodegit");
+
+  Git.Clone("https://github.com/terminal-velocity/patient-track-data.git", "patient-track-data").then(function(repository) {
+    // Work with the repository object here.
+
+  });
+});
 
 module.exports = router;
--- routes/editor.js
+++ routes/editor.js
@@ -1,14 +1,28 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/:form/:id', function(req, res) {
+  var data = "";
+  if(fs.existsSync("./data/patients/"+req.params.id+".json")){
+    console.log("File found")
+    data = fs.readFileSync("./data/patients/"+req.params.id+".json");
+    data = data.toString().replace(/(?:\r\n|\r|\n)/g, '')
+  }
+  console.log(data);
+  res.render('form', {
+    formid: req.params.form,
+    actualid: req.params.id,
+    existingdata: data
+  });
+});
+
+router.post('/:form/:id', function(req, res) {
   res.render('form', {
     formid: req.params.form,
     existingdata: req.params.id
   });
 });
-
-
 
 module.exports = router;
